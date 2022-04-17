1da6b16d81080345639ea33519fe3fe5675ca8e2
caioliveira277 <caio-henrique718@outlook.com>
2022-04-17 16:23:01 -0300

    moving HttpPostClientSpy to its own file

---
+++ src/data/test/mock-http-client.ts
@@ -0,0 +1,10 @@
+import { HttpPostClient } from '../protocols/http/http-post-client';
+
+export class HttpPostClientSpy implements HttpPostClient {
+  url?: string;
+
+  post(url: string): Promise<void> {
+    this.url = url;
+    return Promise.resolve();
+  }
+}
