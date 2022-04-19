5a5e446aecfec52adb7a4e40413c6b2524303829
caioliveira277 <caio-henrique718@outlook.com>
2022-04-19 20:02:05 -0300

    adding authentication view

---
+++ src/presentation/views/authentication/authentication-view.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ButtonComponent, TextInputComponent } from '@/presentation/components';
+import { AuthenticationViewModel } from '@/presentation/view-models';
+import { Container, Content, Ilustation, Title, spacing } from './styles';
+import initialIlustration from '@assets/initial-ilustration.png';
+
+export interface AuthenticationViewProps {
+  authenticationViewModel: AuthenticationViewModel;
+}
+
+export class AuthenticationView extends React.Component<AuthenticationViewProps> {
+  render() {
+    return (
+      <Container>
+        <Ilustation source={initialIlustration} resizeMode="cover" />
+        <Content>
+          <Title style={spacing.title}>Entre com a sua conta</Title>
+          <TextInputComponent
+            style={spacing.input}
+            iconeName="mail"
+            label="Email cadastrado:"
+            placeholderText="Email cadastrado"
+          />
+          <TextInputComponent
+            iconeName="lock"
+            type="password"
+            label="Senha cadastrada:"
+            placeholderText="Senha secreta"
+          />
+          <ButtonComponent style={spacing.button}>Entrar</ButtonComponent>
+        </Content>
+      </Container>
+    );
+  }
+}
---
+++ src/presentation/views/authentication/styles.ts
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.contrast};
+`;
+
+export const Title = styled.Text`
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.secondary};
+  font-family: ${({ theme }) => theme.fonts.families.medium};
+  text-align: center;
+`;
+
+export const Ilustation = styled.Image`
+  width: 100%;
+  height: 362px;
+`;
+
+export const Content = styled(Container)`
+  padding: 0 20px;
+`;
+
+export const spacing = StyleSheet.create({
+  title: {
+    marginBottom: 30,
+  },
+  input: {
+    marginBottom: 25,
+  },
+  button: {
+    marginTop: 40,
+    marginBottom: 40,
+  },
+});
