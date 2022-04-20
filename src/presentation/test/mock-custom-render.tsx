644571d010bdb8c9a067c75fa866e367ecf548ec
caioliveira277 <caio-henrique718@outlook.com>
2022-04-20 15:55:41 -0300

    adding custom render mock

---
+++ src/presentation/test/mock-custom-render.tsx
@@ -0,0 +1,12 @@
+import { render, RenderOptions } from '@testing-library/react-native';
+import { ThemeProvider } from 'styled-components';
+import { currentTheme } from '@/presentation/theme';
+
+const WrapperComponent: React.FC = ({ children }) => (
+  <ThemeProvider theme={currentTheme}>{children}</ThemeProvider>
+);
+
+export const mockRender = (
+  component: React.ReactElement<any>,
+  options?: RenderOptions
+) => render(component, { wrapper: WrapperComponent, ...options });
