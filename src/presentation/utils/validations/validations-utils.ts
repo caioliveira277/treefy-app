f258e1d900364bb77e21e346040b6b108a69f83a
caioliveira277 <caio-henrique718@outlook.com>
2022-05-02 22:24:57 -0300

    chore: add validate email utils function

---
+++ src/presentation/utils/validations/validations-utils.ts
@@ -0,0 +1,7 @@
+export type ValidateFunction = (text: string) => boolean;
+
+export const validateEmail: ValidateFunction = (string) => {
+  const regexEmail =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regexEmail.test(string);
+};
