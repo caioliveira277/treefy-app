2dba7a9b85803f341a1113b71543c9d410b80bc9
caioliveira277 <caio-henrique718@outlook.com>
2022-05-02 22:50:32 -0300

    chore: add validate strong password utils function

--- src/presentation/utils/validations/validations-utils.ts
+++ src/presentation/utils/validations/validations-utils.ts
@@ -1,7 +1,20 @@
 export type ValidateFunction = (text: string) => boolean;
 
-export const validateEmail: ValidateFunction = (string) => {
+export const validateEmail: ValidateFunction = (text) => {
   const regexEmail =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regexEmail.test(string);
+  return regexEmail.test(text);
+};
+
+export const validateStrongPassword: ValidateFunction = (text: string) => {
+  const regexUppercaseLetters = /[A-Z]/g;
+  const regexLowcaseLetters = /[a-z]/g;
+  const regexNumbers = /[0-9]/g;
+
+  return (
+    regexUppercaseLetters.test(text) &&
+    regexLowcaseLetters.test(text) &&
+    regexNumbers.test(text) &&
+    text.length > 5
+  );
 };
