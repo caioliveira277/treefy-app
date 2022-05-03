d9817dbe85f6dfc974e816a1915aefa9d85eff1b
caioliveira277 <caio-henrique718@outlook.com>
2022-05-02 22:51:45 -0300

    test: ensure the function validates if is a strong password

--- src/presentation/utils/validations/validation-utils.spec.ts
+++ src/presentation/utils/validations/validation-utils.spec.ts
@@ -1,29 +1,52 @@
 import faker from '@faker-js/faker';
-import { validateEmail } from './validations-utils';
+import { validateEmail, validateStrongPassword } from './validations-utils';
 
 type SutTypes = {
   sut: () => boolean;
 };
 interface MakeSutProps {
-  string: string;
+  text: string;
 }
 
-const makeSut = ({ string }: MakeSutProps): SutTypes => {
-  const sut = () => validateEmail(string);
-  return {
-    sut,
+describe('Validation Email', () => {
+  const makeSut = ({ text }: MakeSutProps): SutTypes => {
+    const sut = () => validateEmail(text);
+    return {
+      sut,
+    };
   };
-};
-
-describe('Validations', () => {
-  it('Should return if email is valid', () => {
+  it('Should return true if email is valid', () => {
     const anyEmail = faker.internet.email();
-    const { sut } = makeSut({ string: anyEmail });
+    const { sut } = makeSut({ text: anyEmail });
     expect(sut()).toBeTruthy();
   });
-  it('Should return if email is invalid', () => {
+  it('Should return false if email is invalid', () => {
     const anyText = faker.datatype.string();
-    const { sut } = makeSut({ string: anyText });
+    const { sut } = makeSut({ text: anyText });
+    expect(sut()).toBeFalsy();
+  });
+});
+
+describe('Validation Strong Password', () => {
+  const makeSut = ({ text }: MakeSutProps): SutTypes => {
+    const sut = () => validateStrongPassword(text);
+    return {
+      sut,
+    };
+  };
+  it('Should return true if password is strong', () => {
+    const anyPassword = faker.internet.password(
+      9,
+      false,
+      /[A-Za-z]/,
+      faker.internet.password(3, false, /[0-9]/)
+    );
+    const { sut } = makeSut({ text: anyPassword });
+    expect(sut()).toBeTruthy();
+  });
+  it('Should return false if password is not strong', () => {
+    const anyPassword = faker.internet.password(4);
+    const { sut } = makeSut({ text: anyPassword });
     expect(sut()).toBeFalsy();
   });
 });
