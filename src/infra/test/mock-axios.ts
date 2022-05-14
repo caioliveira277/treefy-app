d3493c3699c2252ed93627cb8a78eacd4df773c7
caioliveira277 <caio-henrique718@outlook.com>
2022-05-14 17:56:47 -0300

    refactor: change faker deprecated method

--- src/infra/test/mock-axios.ts
+++ src/infra/test/mock-axios.ts
@@ -4,7 +4,7 @@ import faker from '@faker-js/faker';
 export const mockAxios = (): jest.Mocked<typeof axios> => {
   const mockedAxios = axios as jest.Mocked<typeof axios>;
   mockedAxios.post.mockClear().mockResolvedValue({
-    data: faker.random.objectElement({ any: 'object' }),
+    data: faker.helpers.objectValue({ any: 'object' }),
     status: faker.datatype.number(),
   });
   return mockedAxios;
