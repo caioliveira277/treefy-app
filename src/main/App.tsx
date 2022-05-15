7b145b4c9fec963bd79476272f9f078cdf400914
caioliveira277 <caio-henrique718@outlook.com>
2022-05-15 14:08:03 -0300

    chore: add font bold

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -7,12 +7,14 @@ import {
   useFonts,
   RobotoSlab_400Regular,
   RobotoSlab_500Medium,
+  RobotoSlab_700Bold,
 } from '@expo-google-fonts/roboto-slab';
 
 function App() {
   const [fontsLoaded] = useFonts({
     RobotoSlab_400Regular,
     RobotoSlab_500Medium,
+    RobotoSlab_700Bold,
   });
 
   return fontsLoaded ? (
