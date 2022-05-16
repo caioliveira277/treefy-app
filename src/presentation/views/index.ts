08aaac4f8b60ffaba6c3dbfd1c791a3d6ba4ce83
caioliveira277 <caio-henrique718@outlook.com>
2022-05-15 22:55:01 -0300

    feat: add change password view

--- src/presentation/views/index.ts
+++ src/presentation/views/index.ts
@@ -1,3 +1,4 @@
 export * from './authentication/authentication-view';
 export * from './access/access-view';
 export * from './email-confirmation/email-confirmation-view';
+export * from './change-password/change-password-view';
