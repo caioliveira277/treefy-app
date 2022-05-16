9f115a3631df1e481ca8cdad296f4a094a146855
caioliveira2 <caio.oliveira@ilia.digital>
2022-05-16 16:56:27 -0300

    chore: add slide control component

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,3 +1,4 @@
 export * from './button/button-component';
 export * from './text-input/text-input-component';
 export * from './link/link-component';
+export * from './slide-control/slide-control-component';
---
+++ src/presentation/components/slide-control/slide-control-component.tsx
@@ -0,0 +1,31 @@
+import { StyleProp, ViewStyle } from 'react-native';
+import { useTheme } from 'styled-components';
+import { Container, Point } from './styles';
+
+export interface SlideControlProps {
+  onPress?: () => void;
+  countOfItems: number;
+  activeSlideIndex?: number;
+  style?: StyleProp<ViewStyle>;
+}
+
+export const SlideControlComponent: React.FC<SlideControlProps> = ({
+  countOfItems,
+  activeSlideIndex = 0,
+  style,
+}) => {
+  const theme = useTheme();
+  return (
+    <Container style={style}>
+      {Array(countOfItems)
+        .fill(null)
+        .map((_v, index) => (
+          <Point
+            theme={theme}
+            active={activeSlideIndex === index}
+            key={index}
+          />
+        ))}
+    </Container>
+  );
+};
