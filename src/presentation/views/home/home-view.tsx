2bfa0a2bd546ef9c5fa2242c959cd17853f8a22c
caioliveira277 <caio-henrique718@outlook.com>
2022-05-17 02:14:22 -0300

    feat: add home view

---
+++ src/presentation/views/home/home-view.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { HomeViewModel } from '@/presentation/view-models';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { BaseView } from '../base-view';
+
+export interface HomeViewProps
+  extends NativeStackScreenProps<StackParamList, 'Home'> {
+  homeViewModel: HomeViewModel;
+}
+
+export interface HomeViewState {}
+
+export class HomeView
+  extends React.Component<HomeViewProps, HomeViewState>
+  implements BaseView<HomeViewProps>
+{
+  private homeViewModel: HomeViewModel;
+
+  constructor(props: HomeViewProps) {
+    super(props);
+
+    const { homeViewModel } = this.props;
+    this.homeViewModel = homeViewModel;
+  }
+
+  public componentDidMount(): void {
+    this.homeViewModel.attachView(this);
+  }
+
+  public componentWillUnmount(): void {
+    this.homeViewModel.detachView();
+  }
+
+  onViewModelChanged(): void {}
+
+  render() {
+    return (
+      <View>
+        <Text>Home</Text>
+      </View>
+    );
+  }
+}
--- src/presentation/views/index.ts
+++ src/presentation/views/index.ts
@@ -4,3 +4,4 @@ export * from './email-confirmation/email-confirmation-view';
 export * from './change-password/change-password-view';
 export * from './signup/signup-view';
 export * from './introduction/introduction-view';
+export * from './home/home-view';
