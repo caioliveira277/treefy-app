192ce5614acf7f2b174c0451e63a88249525a48d
caioliveira277 <caio-henrique718@outlook.com>
2022-05-29 14:02:23 -0300

    feat: add terms use view

---
+++ src/presentation/views/terms-use/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
+
+export const SubTitle = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.sizes.lg};
+  color: ${({ theme }) => theme.colors.secondary};
+  font-family: ${({ theme }) => theme.fonts.families.medium};
+  margin-bottom: 20px;
+`;
+
+export const Paragraph = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.sizes.sm};
+  color: ${({ theme }) => theme.colors.body};
+  font-family: ${({ theme }) => theme.fonts.families.regular};
+`;
+
+export const spacing = StyleSheet.create({
+  paragraph: {
+    marginBottom: 20,
+  },
+  button: {
+    marginTop: 40,
+    marginBottom: 40,
+  },
+});
