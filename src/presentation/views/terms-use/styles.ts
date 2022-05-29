3c9bebd13cbac437a7fcf2aacf50abafca6db31c
caioliveira277 <caio-henrique718@outlook.com>
2022-05-29 14:04:04 -0300

    feat: change margin

--- src/presentation/views/terms-use/styles.ts
+++ src/presentation/views/terms-use/styles.ts
@@ -5,7 +5,7 @@ export const SubTitle = styled.Text`
   font-size: ${({ theme }) => theme.fonts.sizes.lg};
   color: ${({ theme }) => theme.colors.secondary};
   font-family: ${({ theme }) => theme.fonts.families.medium};
-  margin-bottom: 20px;
+  margin-bottom: 10px;
 `;
 
 export const Paragraph = styled.Text`
@@ -16,7 +16,7 @@ export const Paragraph = styled.Text`
 
 export const spacing = StyleSheet.create({
   paragraph: {
-    marginBottom: 20,
+    marginBottom: 30,
   },
   button: {
     marginTop: 40,
