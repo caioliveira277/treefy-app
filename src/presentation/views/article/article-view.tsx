f599b16aeeca1d33d02799bac026142e3d430f3f
caioliveira277 <caio-henrique718@outlook.com>
2022-05-30 02:19:20 -0300

    feat: add scroll view container

--- src/presentation/views/article/article-view.tsx
+++ src/presentation/views/article/article-view.tsx
@@ -2,22 +2,31 @@ import React from 'react';
 import { ArticleViewModel } from '@/presentation/view-models';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { BaseView } from '../base-view';
-import { Container } from './styles';
+import { Container, SafeContainer } from './styles';
 import { WebView } from 'react-native-webview';
+import { Dimensions } from 'react-native';
 
 export interface ArticleViewProps
   extends NativeStackScreenProps<StackParamList, 'Article'> {
   articleViewModel: ArticleViewModel;
 }
 
+export interface ArticleViewState {
+  webheight: number;
+}
+
 export class ArticleView
-  extends React.Component<ArticleViewProps>
+  extends React.Component<ArticleViewProps, ArticleViewState>
   implements BaseView<ArticleViewProps>
 {
   private articleViewModel: ArticleViewModel;
 
   constructor(props: ArticleViewProps) {
     super(props);
+
+    this.state = {
+      webheight: Dimensions.get('window').height,
+    };
 
     const { articleViewModel } = this.props;
     this.articleViewModel = articleViewModel;
@@ -38,13 +47,26 @@ export class ArticleView
       <Container>
         <WebView
           style={{
-            flex: 1,
+            height: this.state.webheight,
           }}
           source={{
             uri: 'https://caioliveira277.github.io/todo-list/public/assets/temp/article-example.html',
           }}
-          scalesPageToFit={false}
+          onMessage={(event) => {
+            this.setState({
+              webheight: parseInt(event.nativeEvent.data),
+            });
+          }}
+          scrollEnabled={false}
+          javaScriptEnabled={true}
+          injectedJavaScript={`
+              setTimeout(function() { 
+                window.ReactNativeWebView.postMessage(document.documentElement.scrollHeight); 
+              }, 500);
+              true;
+            `}
         />
+        <SafeContainer></SafeContainer>
       </Container>
     );
   }
--- src/presentation/views/article/styles.ts
+++ src/presentation/views/article/styles.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
   flex: 1;
   padding-top: 40px;
   background-color: ${({ theme }) => theme.colors.contrast};
+`;
+
+export const SafeContainer = styled.View`
+  margin-bottom: 80px;
 `;
