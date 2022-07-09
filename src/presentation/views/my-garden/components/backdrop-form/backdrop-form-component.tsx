91cdee8f502b18f3815cc56799fbddb90a6ae017
caioliveira277 <caio-henrique718@outlook.com>
2022-07-09 15:50:07 -0300

    feat: add form in bottom sheet modal

--- src/presentation/views/my-garden/components/backdrop-form/backdrop-form-component.tsx
+++ src/presentation/views/my-garden/components/backdrop-form/backdrop-form-component.tsx
@@ -1,8 +1,26 @@
-import { useEffect, useMemo, useRef, useCallback } from 'react';
+import { useEffect, useMemo, useRef, useCallback, useState } from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
 import { ModalState } from '@/presentation/@types/generics';
-import { Container, DarkMask, styles } from './styles';
 import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
-import { StyleProp, Text, ViewStyle } from 'react-native';
+import {
+  ButtonComponent,
+  LegendComponent,
+  TextInputComponent,
+  PickerComponent,
+  PickerItemComponent,
+  PickerComponentItemValue,
+} from '@/presentation/components';
+import {
+  Container,
+  bottomSheetStyles,
+  Title,
+  Description,
+  TextRed,
+  styles,
+  CustomLabel,
+  CustomLabelSmall,
+  PeriodContainer,
+} from './styles';
 
 export interface BackdropFormComponentProps {
   style?: StyleProp<ViewStyle>;
@@ -10,40 +28,139 @@ export interface BackdropFormComponentProps {
   onClose?: (closeState: ModalState) => void;
 }
 
+type Period = { label: string; value: string };
+const periods: Period[] = [
+  { label: 'Selecione o período', value: '' },
+  { label: 'Dias', value: 'days' },
+  { label: 'Semanas', value: 'weeks' },
+  { label: 'Meses', value: 'months' },
+  { label: 'Anos', value: 'years' },
+];
+
 export const BackdropFormComponent: React.FC<BackdropFormComponentProps> = ({
   modalState,
   onClose = () => null,
 }) => {
+  const isOpen = () => modalState === ModalState.open;
   const bottomSheetRef = useRef<BottomSheet>(null);
   const snapPoints = useMemo(() => ['50%', '95%'], []);
+  const [sunPeriod, setSunPeriod] = useState<PickerComponentItemValue>(
+    periods[0].value
+  );
+  const [waterPeriod, setWaterPeriod] = useState<PickerComponentItemValue>(
+    periods[0].value
+  );
+
+  useEffect(() => {
+    if (isOpen()) {
+      bottomSheetRef.current?.snapToIndex(1);
+    }
+  }, [modalState]);
 
   const handleClose = useCallback(() => {
     onClose(ModalState.close);
   }, []);
 
-  useEffect(() => {
-    if (modalState === ModalState.open) {
-      bottomSheetRef.current?.snapToIndex(1);
-    }
-  }, [modalState]);
+  const renderPeriodsItem = useCallback(() => {
+    return periods.map((period, index) => (
+      <PickerItemComponent {...period} key={index} />
+    ));
+  }, []);
 
   return (
-    <>
-      {modalState === ModalState.open ? <DarkMask /> : null}
-      <BottomSheet
-        ref={bottomSheetRef}
-        snapPoints={snapPoints}
-        enablePanDownToClose
-        index={-1}
-        onClose={handleClose}
-        style={styles.bottomSheetContainer}
-      >
-        <BottomSheetScrollView style={styles.bottomSheetScrollView}>
-          <Container>
-            <Text>Teste</Text>
-          </Container>
-        </BottomSheetScrollView>
-      </BottomSheet>
-    </>
+    <BottomSheet
+      ref={bottomSheetRef}
+      snapPoints={snapPoints}
+      enablePanDownToClose
+      index={-1}
+      onClose={handleClose}
+      containerStyle={{
+        backgroundColor: isOpen() ? '#00000040' : 'transparent',
+      }}
+      handleIndicatorStyle={bottomSheetStyles.indicator}
+    >
+      <BottomSheetScrollView style={bottomSheetStyles.bottomSheetScrollView}>
+        <Container>
+          <Title>Nova planta</Title>
+          <Description>
+            Os campos obrigatórios estão marcados com um “<TextRed>*</TextRed>”,
+            se ainda não sabe muito sobre a sua nova planta, não se preocupe!
+            Você pode adicionar as outras informações a qualquer momento.
+          </Description>
+          <LegendComponent>Sobre a planta:</LegendComponent>
+          <TextInputComponent
+            label={
+              <>
+                Nome da planta: <TextRed>*</TextRed>
+              </>
+            }
+            iconName="plant-active"
+            iconSize={18}
+            placeholderText="Nome da plantinha"
+            style={styles.input}
+          />
+          <TextInputComponent
+            label={
+              <>
+                Anotações: <TextRed>*</TextRed>
+              </>
+            }
+            iconName="content"
+            type="textarea"
+            iconSize={15}
+            placeholderText="Caracteristicas, cor..."
+            style={styles.input}
+          />
+          <TextInputComponent
+            label="Espécie:"
+            iconName="search"
+            iconSize={16}
+            placeholderText="Encontre a espécie"
+            style={styles.input}
+          />
+          <LegendComponent>Exposição ao sol:</LegendComponent>
+          <CustomLabel>De quanto em quanto tempo?</CustomLabel>
+          <PeriodContainer>
+            <CustomLabelSmall>A cada:</CustomLabelSmall>
+            <TextInputComponent
+              iconName="sun"
+              iconSize={16}
+              placeholderText="0"
+              style={styles.inputPeriodNumberContainer}
+              styleInput={styles.inputPeriodNumber}
+              keyboardType="number-pad"
+            />
+            <PickerComponent
+              selectedValue={sunPeriod}
+              onValueChange={(itemValue) => setSunPeriod(itemValue)}
+            >
+              {renderPeriodsItem()}
+              <PickerItemComponent />
+            </PickerComponent>
+          </PeriodContainer>
+          <LegendComponent>Regagem:</LegendComponent>
+          <CustomLabel>De quanto em quanto tempo?</CustomLabel>
+          <PeriodContainer>
+            <CustomLabelSmall>A cada:</CustomLabelSmall>
+            <TextInputComponent
+              iconName="water-drop"
+              iconSize={16}
+              placeholderText="0"
+              style={styles.inputPeriodNumberContainer}
+              styleInput={styles.inputPeriodNumber}
+              keyboardType="number-pad"
+            />
+            <PickerComponent
+              selectedValue={waterPeriod}
+              onValueChange={(itemValue) => setWaterPeriod(itemValue)}
+            >
+              {renderPeriodsItem()}
+              <PickerItemComponent />
+            </PickerComponent>
+          </PeriodContainer>
+          <ButtonComponent style={styles.button}>Salvar</ButtonComponent>
+        </Container>
+      </BottomSheetScrollView>
+    </BottomSheet>
   );
 };
--- src/presentation/views/my-garden/components/backdrop-form/styles.ts
+++ src/presentation/views/my-garden/components/backdrop-form/styles.ts
@@ -1,25 +1,67 @@
 import { StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 import { currentTheme } from '@/presentation/themes';
+import { Label } from '@/presentation/components/text-input/styles';
 
 export const Container = styled.View`
-  padding: 0px 20px;
+  padding: 40px 20px;
 `;
 
-export const DarkMask = styled.View`
-  background-color: #00000040;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+export const PeriodContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 30px;
 `;
 
-export const styles = StyleSheet.create({
-  bottomSheetContainer: {
-    borderRadius: 20,
-  },
+export const Title = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.sizes.xl};
+  color: ${({ theme }) => theme.colors.secondary};
+  font-family: ${({ theme }) => theme.fonts.families.medium};
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+export const Description = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.sizes.sm};
+  font-family: ${({ theme }) => theme.fonts.families.regular};
+  color: ${({ theme }) => theme.colors.body};
+  margin-bottom: 30px;
+`;
+
+export const TextRed = styled(Description)`
+  color: red;
+`;
+
+export const CustomLabel = styled(Label)``;
+
+export const CustomLabelSmall = styled(Label)`
+  font-size: ${({ theme }) => theme.fonts.sizes.sm};
+  margin: 0;
+`;
+
+export const bottomSheetStyles = StyleSheet.create({
   bottomSheetScrollView: {
     backgroundColor: currentTheme.colors.contrast,
+  },
+  indicator: {
+    backgroundColor: currentTheme.colors.primary,
+    width: 40,
+  },
+});
+
+export const styles = StyleSheet.create({
+  input: {
+    marginBottom: 25,
+  },
+  inputPeriodNumberContainer: {
+    width: 80,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  inputPeriodNumber: {
+    paddingRight: 0,
+  },
+  button: {
+    marginTop: 10,
   },
 });
