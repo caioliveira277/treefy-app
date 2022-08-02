fb32b6f666df602c16e4770f3e7d502aeda1169e
caioliveira <caio-henrique718@outlook.com>
2022-08-02 19:37:49 -0300

    chore: implement methos in identify provider

--- src/data/protocols/identity/identity-provider.ts
+++ src/data/protocols/identity/identity-provider.ts
@@ -1,8 +1,15 @@
 import { AccountModel } from '@/domain/models';
-import { AuthenticationParams, SignupParams } from '@/domain/usecases';
+import {
+  AuthenticationParams,
+  ConfirmByCodeParms,
+  SendConfirmationCodeParams,
+  SignupParams,
+} from '@/domain/usecases';
 
 export interface IdentityProvider {
   configure(): void;
   signup(params: SignupParams): Promise<boolean>;
   signin(params: AuthenticationParams): Promise<AccountModel>;
+  confirmSignup(params: ConfirmByCodeParms): Promise<boolean>;
+  sendConfirmationCode(params: SendConfirmationCodeParams): Promise<boolean>;
 }
