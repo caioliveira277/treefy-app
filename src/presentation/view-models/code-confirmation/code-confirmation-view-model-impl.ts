6ca42165b972534d3ee3b82b54c38cd9889531b3
caioliveira <caio-henrique718@outlook.com>
2022-08-06 13:18:14 -0300

    feat: implement code confirmation

--- src/presentation/view-models/code-confirmation/code-confirmation-view-model-impl.ts
+++ src/presentation/view-models/code-confirmation/code-confirmation-view-model-impl.ts
@@ -2,16 +2,23 @@ import { validateConfirmationCode } from '@/validations/validations';
 import { Alert } from 'react-native';
 import { CodeConfirmationViewModel } from './code-confirmation-view-model';
 import { BaseViewModelImpl } from '../base-view-model-impl';
+import { Authentication, Signup } from '@/domain/usecases';
 
 export class CodeConfirmationViewModelImpl
   extends BaseViewModelImpl
   implements CodeConfirmationViewModel
 {
+  public signup: Signup;
+
+  public authentication: Authentication;
+
   public codeValue: string;
 
-  public constructor() {
+  public constructor(signup: Signup, authentication: Authentication) {
     super();
     this.codeValue = '';
+    this.signup = signup;
+    this.authentication = authentication;
   }
 
   public handleCodeInputChange(value: string): void {
@@ -19,15 +26,33 @@ export class CodeConfirmationViewModelImpl
     this.notifyViewAboutChanges();
   }
 
-  public handleSubmit(): void {
-    const emailValid = validateConfirmationCode(this.codeValue);
+  public async handleSubmit(): Promise<void> {
+    const codeValid = validateConfirmationCode(this.codeValue);
+    const params = this.baseView?.props.route
+      .params as StackParamList['CodeConfirmation'];
 
-    if (!emailValid) {
-      Alert.alert('Ops!', 'Invalid code');
-    } else {
-      this.baseView?.props.navigation.navigate('Public', {
-        screen: 'Access',
-      });
+    if (!codeValid) {
+      return Alert.alert('Ops!', 'Invalid code');
+    }
+
+    const confirmedCode = await this.signup.confirmByCode({
+      email: params.email,
+      code: this.codeValue,
+    });
+
+    if (!confirmedCode) Alert.alert('Error!', 'failed to confirm code');
+
+    switch (params.flow) {
+      case 'Signup':
+        Alert.alert('Success!', 'Account confimed successfully');
+        await this.authentication.auth({
+          email: params.email,
+          password: params.password || '',
+        });
+        this.baseView?.props.navigation.navigate('Main', {
+          screen: 'Home',
+        });
+        break;
     }
   }
 }
