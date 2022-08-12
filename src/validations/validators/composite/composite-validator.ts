94af5b491d183ea09f6826376e5d687448c3f469
caioliveira <caio-henrique718@outlook.com>
2022-08-12 20:17:45 -0300

    chore: create composite validator applying composite design pattern

---
+++ src/validations/validators/composite/composite-validator.ts
@@ -0,0 +1,37 @@
+import { Validation } from '@/presentation/protocols/validation';
+import { FieldValidation } from '@/validations/protocols/field-validation';
+
+export class CompositeValidator implements Validation {
+  private readonly validator: FieldValidation[];
+
+  private constructor(validator: FieldValidation[]) {
+    this.validator = validator;
+  }
+
+  static build(validator: FieldValidation[]): CompositeValidator {
+    return new CompositeValidator(validator);
+  }
+
+  public validate(fieldName: string, object: Record<string, any>): string {
+    const validators = this.validator.filter((v) => v.field === fieldName);
+    for (const validator of validators) {
+      const error = validator.validate(object);
+      if (error) {
+        return error.message;
+      }
+    }
+    return '';
+  }
+
+  public validateAll(
+    fieldNames: string[],
+    object: Record<string, string>
+  ): { errors: Record<string, string>; hasError: boolean } {
+    const result = { errors: { ...object }, hasError: false };
+    fieldNames.forEach((fieldName) => {
+      result.errors[fieldName] = this.validate(fieldName, object);
+      if (result.errors[fieldName]) result.hasError = true;
+    });
+    return result;
+  }
+}
