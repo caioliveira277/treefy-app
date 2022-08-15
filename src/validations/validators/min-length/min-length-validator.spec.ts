7a50b1408e6db8e0bf00dfb60d0523ecb8321a58
caioliveira <caio-henrique718@outlook.com>
2022-08-15 16:42:28 -0300

    test: ensure minLength validator return correct validations

---
+++ src/validations/validators/min-length/min-length-validator.spec.ts
@@ -0,0 +1,43 @@
+import { MinLengthFieldError } from '@/validations/errors';
+import { MinLengthValidator } from './min-length-validator';
+import faker from '@faker-js/faker';
+
+interface MakeSut {
+  (params: { field: string; minLength: number }): { sut: MinLengthValidator };
+}
+
+const makeSut: MakeSut = ({ field, minLength }) => ({
+  sut: new MinLengthValidator(field, minLength),
+});
+
+describe('MinLengthValidator', () => {
+  test('Should return error if string is less than minLength param', () => {
+    const field = faker.database.column();
+    const minLength = faker.datatype.number({ min: 1, max: 150 });
+    const { sut } = makeSut({ field, minLength });
+    const error = sut.validate({
+      [field]: faker.datatype.string(minLength - 2),
+    });
+    expect(error).toEqual(new MinLengthFieldError(minLength));
+  });
+
+  test('Should return falsy if string is greater than minLength param', () => {
+    const field = faker.database.column();
+    const minLength = faker.datatype.number({ min: 1, max: 150 });
+    const { sut } = makeSut({ field, minLength });
+    const error = sut.validate({
+      [field]: faker.datatype.string(minLength + 2),
+    });
+    expect(error).toBeFalsy();
+  });
+
+  test('Should return falsy if string is empty', () => {
+    const field = '';
+    const minLength = faker.datatype.number({ min: 1, max: 150 });
+    const { sut } = makeSut({ field, minLength });
+    const error = sut.validate({
+      [field]: faker.datatype.string(minLength + 2),
+    });
+    expect(error).toBeFalsy();
+  });
+});
