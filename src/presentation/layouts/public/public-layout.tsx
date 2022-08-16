37901682e4a11985608f663685eb625ac60ba948
caioliveira <caio-henrique718@outlook.com>
2022-08-16 12:44:57 -0300

    feat: add loading component in public layout

--- src/presentation/layouts/public/public-layout.tsx
+++ src/presentation/layouts/public/public-layout.tsx
@@ -1,24 +1,30 @@
 import { Container, Content, Ilustation, Title, Paragraph } from './styles';
 import palmTreesIlustration from '@assets/images/palm-trees-ilustration.png';
+import { PageLoadingComponent } from '@/presentation/components';
 
 export interface PublicLayoutProps {
   children: React.ReactNode;
   title: string;
   paragraph?: string;
+  isLoading?: boolean;
 }
 export const PublicLayout: React.FC<PublicLayoutProps> = ({
   children,
   title,
   paragraph,
+  isLoading = false,
 }) => {
   return (
-    <Container>
-      <Ilustation source={palmTreesIlustration} resizeMode="cover" />
-      <Content>
-        <Title>{title}</Title>
-        {paragraph ? <Paragraph>{paragraph}</Paragraph> : null}
-        {children}
-      </Content>
-    </Container>
+    <>
+      <Container>
+        <Ilustation source={palmTreesIlustration} resizeMode="cover" />
+        <Content>
+          <Title>{title}</Title>
+          {paragraph ? <Paragraph>{paragraph}</Paragraph> : null}
+          {children}
+        </Content>
+      </Container>
+      {isLoading ? <PageLoadingComponent /> : null}
+    </>
   );
 };
