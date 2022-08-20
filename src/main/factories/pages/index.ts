6eff44237fd333812516574d78c90928a157a420
caioliveira <caio-henrique718@outlook.com>
2022-08-20 11:16:33 -0300

    refactor: change page folder name to view

--- src/main/factories/pages/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './access/access-page-factory';
--- src/main/factories/views/access/access-view-factory.tsx
+++ src/main/factories/views/access/access-view-factory.tsx
@@ -2,12 +2,12 @@ import { AccessViewModelImpl } from '@/presentation/view-models';
 import { AccessView } from '@/presentation/views';
 import { RouteProp } from '@react-navigation/native';
 
-interface MakeAccessPageProps {
+interface MakeAccessViewProps {
   route: RouteProp<StackParamList, 'Access'>;
   navigation: any;
 }
 
-export const makeAccessPage: React.FC<MakeAccessPageProps> = (props) => {
+export const makeAccessView: React.FC<MakeAccessViewProps> = (props) => {
   const accessViewModel = new AccessViewModelImpl();
   return <AccessView {...props} accessViewModel={accessViewModel} />;
 };
