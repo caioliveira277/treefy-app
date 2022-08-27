990f40bddae891839d15180e0999442da7de4497
caioliveira <caio-henrique718@outlook.com>
2022-08-27 09:17:45 -0300

    feat: add loading in profile layout

--- src/presentation/layouts/profile/profile-layout.tsx
+++ src/presentation/layouts/profile/profile-layout.tsx
@@ -1,6 +1,8 @@
 import { Container, Content, Ilustation, Title, Image, Header } from './styles';
 import profileHeaderLayout from '@assets/images/profile-header-background.png';
 import { ImageSourcePropType } from 'react-native';
+import { useEffect, useState } from 'react';
+import { PageLoadingComponent } from '@/presentation/components';
 
 export interface ProfileLayoutProps {
   children: React.ReactNode;
@@ -8,6 +10,7 @@ export interface ProfileLayoutProps {
   title: string;
   image: ImageSourcePropType;
   imageRounded?: boolean;
+  isLoading: boolean;
 }
 export const ProfileLayout: React.FC<ProfileLayoutProps> = ({
   children,
@@ -15,22 +18,38 @@ export const ProfileLayout: React.FC<ProfileLayoutProps> = ({
   title,
   image,
   imageRounded = false,
+  isLoading = false,
 }) => {
+  const [loading, setLoading] = useState(isLoading);
+
+  useEffect(() => {
+    if (!isLoading) {
+      setTimeout(() => {
+        setLoading(isLoading);
+      }, 500);
+    } else {
+      setLoading(isLoading);
+    }
+  }, [isLoading]);
+
   return (
-    <Container>
-      <Ilustation source={profileHeaderLayout} resizeMode="cover" />
-      <Content>
-        <Header>
-          <Image
-            source={image}
-            resizeMode="center"
-            imageRounded={imageRounded}
-          />
-          <Title>{title}</Title>
-          {slotHeader || null}
-        </Header>
-        {children}
-      </Content>
-    </Container>
+    <>
+      <Container>
+        <Ilustation source={profileHeaderLayout} resizeMode="cover" />
+        <Content>
+          <Header>
+            <Image
+              source={image}
+              resizeMode="center"
+              imageRounded={imageRounded}
+            />
+            <Title>{title}</Title>
+            {slotHeader || null}
+          </Header>
+          {children}
+        </Content>
+      </Container>
+      {loading ? <PageLoadingComponent /> : null}
+    </>
   );
 };
