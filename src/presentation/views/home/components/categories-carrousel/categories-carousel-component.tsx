5907584fb373ab186261a5771e991857671bd610
caioliveira <caio-henrique718@outlook.com>
2022-09-10 13:43:39 -0300

    feat: integrate categories listing in home view

--- src/presentation/views/home/components/categories-carrousel/categories-carousel-component.tsx
+++ src/presentation/views/home/components/categories-carrousel/categories-carousel-component.tsx
@@ -10,58 +10,71 @@ import {
   ActivePoint,
   customStyle,
 } from './styles';
-import categoryCactusImage from '@assets/images/category-cactus.png';
-import categoryPlantsImage from '@assets/images/category-plants.png';
 import { StyleProp, ViewStyle } from 'react-native';
+import { CategoryModel } from '@/domain/models';
+import { useEffect, useState } from 'react';
 
-const temporaryData = [
-  {
-    title: 'Cactos',
-    image: categoryCactusImage,
-    active: true,
-  },
-  {
-    title: 'Alfaces',
-    image: categoryPlantsImage,
-    active: false,
-  },
-  {
-    title: 'Alfaces',
-    image: categoryPlantsImage,
-    active: false,
-  },
-  {
-    title: 'Alfaces',
-    image: categoryPlantsImage,
-    active: false,
-  },
-  {
-    title: 'Alfaces',
-    image: categoryPlantsImage,
-    active: false,
-  },
-];
+type ListItem = {
+  id: number;
+  title: string;
+  image: string;
+  active: boolean;
+};
 
 export interface CategoriesCarrouselComponentProps {
   style?: StyleProp<ViewStyle>;
+  categories: CategoryModel[];
 }
 
 export const CategoriesCarrouselComponent: React.FC<
   CategoriesCarrouselComponentProps
-> = ({ style }) => {
+> = ({ style, categories }) => {
   const theme = useTheme();
+  const [list, setList] = useState<ListItem[]>([]);
+
+  const handleFormatCategories = (categoriesData: CategoryModel[]) => {
+    setList(
+      categoriesData.map((category, index) => ({
+        id: category.id,
+        active: index === 0,
+        title: category.title,
+        image: category.image,
+      }))
+    );
+  };
+
+  const handleSelectItem = (id: number) => {
+    const newState = list.map((item) => {
+      item.active = false;
+      if (item.id === id) item.active = true;
+      return item;
+    });
+    setList(newState);
+  };
+
+  useEffect(() => {
+    handleFormatCategories(categories);
+  }, [categories]);
   return (
     <Container style={style}>
       <Title>Categorias</Title>
       <Corrousel horizontal={true} showsHorizontalScrollIndicator={false}>
-        {temporaryData.map((item, index) => (
+        {list.map((item, index) => (
           <ItemContainer
             active={item.active}
-            style={item.active ? customStyle.active : null}
-            key={index}
+            style={!index ? customStyle.firstItem : null}
+            key={item.id}
           >
-            <ContainerShadow style={{ ...theme.shadows.sm }}>
-              <ItemImage source={item.image} resizeMode="center" />
+            <ContainerShadow
+              style={{ ...theme.shadows.sm }}
+              onPress={() => handleSelectItem(item.id)}
+            >
+              <ItemImage
+                source={{
+                  uri: item.image,
+                }}
+                resizeMode="center"
+              />
             </ContainerShadow>
             <ItemText active={item.active}>{item.title}</ItemText>
             {item.active ? <ActivePoint /> : null}
