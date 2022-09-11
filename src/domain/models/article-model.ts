62819e0389b401a97497ca762910d628dc7382e5
caioliveira <caio-henrique718@outlook.com>
2022-09-11 09:35:35 -0300

    chore: add article model

---
+++ src/domain/models/article-model.ts
@@ -0,0 +1,8 @@
+export type ArticleModel = {
+  id: number;
+  title: string;
+  description: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
--- src/domain/models/index.ts
+++ src/domain/models/index.ts
@@ -1,2 +1,3 @@
 export * from './account-model';
 export * from './category-model';
+export * from './article-model';
