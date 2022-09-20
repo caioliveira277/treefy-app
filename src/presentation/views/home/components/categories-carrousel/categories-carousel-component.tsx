f17de5e030e2bafa540c49d52452fe296e440194
caioliveira <caio-henrique718@outlook.com>
2022-09-20 20:09:28 -0300

    refactor: remove method to set initial category id

--- src/presentation/views/home/components/categories-carrousel/categories-carousel-component.tsx
+++ src/presentation/views/home/components/categories-carrousel/categories-carousel-component.tsx
@@ -12,63 +12,27 @@ import {
 } from './styles';
 import { StyleProp, ViewStyle } from 'react-native';
 import { CategoryModel } from '@/domain/models';
-import { useEffect, useRef, useState } from 'react';
 import { CategoriesCarouselLoading } from './categories-carousel-loading';
-
-type ListItem = {
-  id: number;
-  title: string;
-  image: string;
-  active: boolean;
-};
 
 export interface CategoriesCarrouselComponentProps {
   style?: StyleProp<ViewStyle>;
   categories: CategoryModel[];
   loading: boolean;
+  selectedCategoryId: number | null;
   onSelectCategory: (categoryId: number) => void;
 }
 
 export const CategoriesCarrouselComponent: React.FC<
   CategoriesCarrouselComponentProps
-> = ({ style, categories, onSelectCategory, loading }) => {
+> = ({ style, categories, onSelectCategory, loading, selectedCategoryId }) => {
   const theme = useTheme();
-  const [list, setList] = useState<ListItem[]>([]);
-
-  const handleFormatCategories = () => {
-    setList(
-      categories.map((category) => ({
-        id: category.id,
-        active: false,
-        title: category.title,
-        image: category.image,
-      }))
-    );
-  };
 
   const handleSelectItem = (id: number) => {
-    const newState = list.map((item) => {
-      item.active = false;
-      if (item.id === id) item.active = true;
-      return item;
-    });
-    setList(newState);
     onSelectCategory(id);
   };
 
-  useEffect(() => {
-    handleFormatCategories();
-  }, [categories]);
-
-  const calledOnlyOnce = useRef(false);
-  useEffect(() => {
-    if (calledOnlyOnce.current || !list.length) return;
-
-    handleSelectItem(list[0].id);
-    calledOnlyOnce.current = true;
-  }, [list]);
-
-  const isLast = (index: number) => index + 1 === list.length;
+  const isLast = (index: number) => index + 1 === categories.length;
+  const isActive = (id: number) => id === selectedCategoryId;
 
   return (
     <Container style={style}>
@@ -77,9 +41,9 @@ export const CategoriesCarrouselComponent: React.FC<
         {loading ? (
           <CategoriesCarouselLoading />
         ) : (
-          list.map((item, index) => (
+          categories.map((item, index) => (
             <ItemContainer
-              active={item.active}
+              active={isActive(item.id)}
               style={isLast(index) ? customStyle.lastItem : customStyle.item}
               key={item.id}
             >
@@ -94,8 +58,8 @@ export const CategoriesCarrouselComponent: React.FC<
                   resizeMode="center"
                 />
               </ContainerShadow>
-              <ItemText active={item.active}>{item.title}</ItemText>
-              {item.active ? <ActivePoint /> : null}
+              <ItemText active={isActive(item.id)}>{item.title}</ItemText>
+              {isActive(item.id) ? <ActivePoint /> : null}
             </ItemContainer>
           ))
         )}
